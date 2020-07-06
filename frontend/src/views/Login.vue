1692ac760af352b14c0bc69cbf33aa40f6d99663
pxeger <_@pxeger.com>
2020-07-06 17:22:18 +0100

    add login route

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-content app>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card>
+              <v-toolbar flat color="primary">
+                <v-toolbar-title>Login</v-toolbar-title>
+              </v-toolbar>
+              <v-form>
+                <v-card-text>
+                  <v-text-field
+                    label="Email"
+                    name="email"
+                    prepend-icon="mdi-email"
+                    type="text"
+                  ></v-text-field>
+                  <v-text-field
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-asterisk"
+                    type="password"
+                  ></v-text-field>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="accent">Login</v-btn>
+                </v-card-actions>
+              </v-form>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="scss"></style>
