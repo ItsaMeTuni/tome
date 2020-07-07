e7aef80c920bfa9863a7069d9bfc35d2dd807d06
pxeger <_@pxeger.com>
2020-07-07 13:51:40 +0100

    add token refresh feature

--- backend/tome/routes/auth.py
+++ backend/tome/routes/auth.py
@@ -4,6 +4,7 @@ from tome.controllers.auth import get_auth_token
 from tome.controllers.password import verify_password
 from tome.database import connection
 from tome.exceptions import HTTPException
+from tome.middleware.auth import requires
 from tome.responses import ORJSONResponse
 from tome.routing import post
 from tome.utils import get_json, validate_types_raising
@@ -19,7 +20,16 @@ async def login(request: starlette.requests.Request) -> ORJSONResponse:
     )
     if not (row and verify_password(row["password"], json["password"])):
         raise HTTPException("Incorrect username or password", 401)
-    return ORJSONResponse(await get_auth_token(row["id"], ["user/read", "user/write"]))
+    return ORJSONResponse(
+        await get_auth_token(row["id"], ["user/read", "user/write", "refresh"])
+    )
+
+
+@post("/api/auth/refresh")
+@requires("refresh")
+async def refresh(request: starlette.requests.Request) -> ORJSONResponse:
+    """refreshes an auth token"""
+    return ORJSONResponse(await get_auth_token(request.user.id, request.auth))
 
 
 routes = [login]
