de926abf09f1fd0e9f14417c4c1eeadc568baf27
pxeger <_@pxeger.com>
2020-07-07 16:27:04 +0100

    redirect to/from login page appropriately

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -13,11 +14,23 @@ const routes = [
   {
     path: '/login',
     name: 'Login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue'),
+    beforeEnter: (whither, whence, next) => {
+      // go back if they're already logged in
+      if (store.state.login.loginState === true) next(false)
+      else next()
+    }
   }
 ]
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   routes
+})
+export default router
+
+router.beforeEach((whither, whence, next) => {
+  if (store.state.login.loginState !== true && whither.name !== 'Login') {
+    next({ name: 'Login' })
+  } else next()
 })
