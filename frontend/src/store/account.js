7f689ef247d04f8ae6b5817a822c904a8bdec708
pxeger <_@pxeger.com>
2020-07-12 07:44:11 +0100

    rewrite account settings to make better use of store

--- frontend/src/store/account.js
+++ frontend/src/store/account.js
@@ -2,7 +2,10 @@ export default {
   namespaced: true,
   state: {
     name: null,
-    email: null
+    email: null,
+    errors: [],
+    currentPassword: '',
+    newPassword: ''
   },
   mutations: {
     setName (state, value) {
@@ -10,29 +13,92 @@ export default {
     },
     setEmail (state, value) {
       state.email = value
+    },
+    setCurrentPassword (state, value) {
+      state.currentPassword = value
+    },
+    setNewPassword (state, value) {
+      state.newPassword = value
+    },
+    addError (state, error) {
+      state.errors.push(error)
+    },
+    delError (state, idx) {
+      state.errors.splice(idx, 1)
     }
   },
   actions: {
-    changeName ({ commit, dispatch, state }, value) {
-      const previous = state.name
-      commit('setName', value)
-      dispatch('apiRequest', {
-        method: 'PATCH',
-        path: '/api/me/name',
-        data: value
-      }, { root: true }).then((r) => { if (!r.ok) throw new Error() }).catch(() => {
-        commit('setName', previous)
+    changeName ({ dispatch, state, commit }) {
+      dispatch(
+        'apiRequest',
+        { method: 'PATCH', path: '/api/me/name', data: state.name },
+        { root: true }
+      ).then(r => {
+        if (!r.ok) {
+          r.json().then(d => {
+            commit('addError', d.error)
+          })
+        }
+      }).catch(e => {
+        commit('addError', 'An error occurred')
+        console.error(e)
       })
     },
-    changeEmail ({ commit, dispatch, state }, value) {
-      const previous = state.email
-      commit('setEmail', value)
-      dispatch('apiRequest', {
-        method: 'PATCH',
-        path: '/api/me/email',
-        data: value
-      }, { root: true }).then((r) => { if (!r.ok) throw new Error() }).catch(() => {
-        commit('setEmail', previous)
+    changeEmail ({ dispatch, state, commit }) {
+      dispatch(
+        'apiRequest',
+        { method: 'PATCH', path: '/api/me/email', data: state.email },
+        { root: true }
+      ).then(r => {
+        if (!r.ok) {
+          r.json().then(d => {
+            commit('addError', d.error)
+          })
+        }
+      }).catch(e => {
+        commit('addError', 'An error occurred')
+        console.error(e)
+      })
+    },
+    changePassword ({ dispatch, state, commit }) {
+      const data = {
+        new: state.newPassword,
+        current: state.currentPassword
+      }
+      dispatch(
+        'apiRequest',
+        { method: 'POST', path: '/api/me/password', data: data },
+        { root: true }
+      ).then(r => {
+        if (!r.ok) {
+          r.json().then(d => {
+            commit('addError', d.error)
+          })
+        } else {
+          commit('setCurrentPassword', null)
+          commit('setNewPassword', null)
+        }
+      }).catch(e => {
+        commit('addError', 'An error occurred')
+        console.error(e)
+      })
+    },
+    fetchAccountDetails ({ dispatch, commit }) {
+      dispatch(
+        'apiRequest',
+        { method: 'GET', path: '/api/me' },
+        { root: true }
+      ).then(r => {
+        r.json().then(d => {
+          if (!r.ok) commit('addError', d.error)
+          else {
+            commit('setEmail', d.email)
+            commit('setName', d.name)
+          }
+        })
+      }).catch(e => {
+        commit('addError', 'An error occurred')
+        console.error(e)
       })
     }
   }
--- frontend/src/views/settings/Account.vue
+++ frontend/src/views/settings/Account.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
+    <v-alert
+      type="error"
+      v-for="(error, i) in $store.state.account.errors"
+      :key="i"
+      dismissible
+      @input="$event || $store.commit('account/delError', i)"
+    >{{ error }}</v-alert>
     <h1>Account Settings</h1>
-    <section class="mb-8 mt-4">
+    <section class="mt-4">
       <h2 class="mb-2">General</h2>
       <p>Change general settings about your account</p>
-      <v-form lazy-validation @submit.prevent="handleGeneralSubmit">
+      <v-form
+        lazy-validation
+        @submit.prevent="$store.dispatch('account/changeName')"
+        class="d-flex"
+        v-model="nameFormValid"
+      >
         <v-text-field
           label="Name"
           name="name"
@@ -13,7 +25,21 @@
           required
           :rules="[v => !!v || 'This field is required']"
           v-model="name"
-        >{{ name }}</v-text-field>
+        ></v-text-field>
+        <v-btn
+          type="submit"
+          outlined
+          color="info"
+          class="align-self-center ms-3 mt-n8"
+          :disabled="!nameFormValid"
+        >Save</v-btn>
+      </v-form>
+      <v-form
+        lazy-validation
+        @submit.prevent="$store.dispatch('account/changeEmail')"
+        class="d-flex"
+        v-model="emailFormValid"
+      >
         <v-text-field
           label="Email address"
           name="email"
@@ -23,17 +49,38 @@
           :rules="[v => validateEmail(v) || 'Enter a valid email address']"
           v-model="email"
         ></v-text-field>
-        <v-btn type="submit" outlined color="info">Save</v-btn>
+        <v-btn
+          type="submit"
+          outlined color="info"
+          class="align-self-center ms-3 mt-n8"
+          :disabled="!emailFormValid"
+        >Save</v-btn>
       </v-form>
     </section>
-    <section class="my-8">
+    <section class="mb-8">
       <h2 class="mb-2">Password</h2>
       <p>If you wish to change your password, you must first enter your current password.
         Make sure to choose a secure one!</p>
-      <v-form lazy-validation @submit.prevent="handlePasswordSubmit">
-        <Password outlined label="Current Password"></Password>
-        <Password outlined label="New Password"></Password>
-        <v-btn type="submit" outlined color="info">Change Password</v-btn>
+      <v-form
+        lazy-validation
+        @submit.prevent="$store.dispatch('account/changePassword')"
+        v-model="passwordFormValid"
+      >
+        <div class="d-flex flex-wrap mx-n2">
+          <Password
+            outlined
+            label="Current password"
+            class="mx-2"
+            v-model="currentPassword"
+          ></Password>
+          <Password
+            outlined
+            label="New password"
+            class="mx-2"
+            v-model="newPassword"
+          ></Password>
+        </div>
+        <v-btn type="submit" outlined color="info" :disabled="!passwordFormValid">Change Password</v-btn>
       </v-form>
     </section>
     <section class="my-8">
@@ -48,39 +95,63 @@
 <script>
 import { validate } from 'email-validator'
 import Password from '@/components/Password'
+import Store from '@/store'
 
 export default {
   name: 'AccountSettings',
   data: () => ({
-    name: 'Scott Morrison',
-    email: 'scott@gov.au',
-    prevName: 'Scott Morrison',
-    prevEmail: 'scott@gov.au'
+    passwordFormValid: true,
+    emailFormValid: true,
+    nameFormValid: true
   }),
   methods: {
     validateEmail: validate,
-    handleGeneralSubmit () {
-      if (this.name !== this.prevName) {
-        this.$store.dispatch('account/changeName', this.name)
-        this.prevName = this.name
-      }
-      if (this.email !== this.prevEmail) {
-        this.$store.dispatch('account/changeEmail', this.email)
-        this.prevEmail = this.email
-      }
-    },
     handleDeleteAccount () {
       if (confirm('Are you completely sure you want to delete your account? This action' +
         'is completely irreversible! All of your data will be permanently deleted!')) {
         console.log('Deleting account')
       }
-    },
-    handlePasswordSubmit () {
-      alert(1)
     }
   },
   components: {
     Password
+  },
+  computed: {
+    name: {
+      get () {
+        return this.$store.state.account.name
+      },
+      set (val) {
+        this.$store.commit('account/setName', val)
+      }
+    },
+    email: {
+      get () {
+        return this.$store.state.account.email
+      },
+      set (val) {
+        this.$store.commit('account/setEmail', val)
+      }
+    },
+    currentPassword: {
+      get () {
+        return this.$store.state.account.currentPassword
+      },
+      set (val) {
+        this.$store.commit('account/setCurrentPassword', val)
+      }
+    },
+    newPassword: {
+      get () {
+        return this.$store.state.account.newPassword
+      },
+      set (val) {
+        this.$store.commit('account/setNewPassword', val)
+      }
+    }
+  },
+  beforeRouteEnter (whither, whence, next) {
+    Store.dispatch('account/fetchAccountDetails').then(next)
   }
 }
 </script>
