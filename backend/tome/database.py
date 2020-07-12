0f1e9c2988a56e6b3b7c29157df19eb3fdd0469d
pxeger <_@pxeger.com>
2020-07-12 07:31:45 +0100

    fix UUID interpretation in database

--- backend/tome/database.py
+++ backend/tome/database.py
@@ -1,3 +1,6 @@
+import uuid
+from typing import Any
+
 import asyncpg  # type: ignore
 
 from tome import settings
@@ -9,6 +12,20 @@ def connection() -> asyncpg.Connection:
     return _connection
 
 
+def _uuid_coerce(value: Any) -> bytes:
+    # TODO(pxeger) PEP 622
+    if isinstance(value, str):
+        return uuid.UUID(value).bytes
+    elif isinstance(value, bytes):
+        return value
+    elif isinstance(value, uuid.UUID):
+        return value.bytes
+    elif isinstance(value, int):
+        return uuid.UUID(int=value).bytes
+    else:
+        raise TypeError(f"cannot coerce object of type {type(value)} to a UUID")
+
+
 async def connect() -> None:
     global _connection
     if not _connection or not _connection.is_closed():
@@ -18,6 +35,13 @@ async def connect() -> None:
             user=settings.POSTGRES_USER,
             password=settings.POSTGRES_PASSWORD,
             database=settings.POSTGRES_DB,
+        )
+        await _connection.set_type_codec(
+            "uuid",
+            encoder=_uuid_coerce,
+            decoder=lambda u: uuid.UUID(bytes=u),
+            schema="pg_catalog",
+            format="binary"
         )
 
 
