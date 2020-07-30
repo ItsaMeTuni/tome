39ff936e68da69f7b1d57581f6e9333a8e59be00
pxeger <_@pxeger.com>
2020-07-30 10:39:29 +0200

    add login refresh token every 5 minutes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,18 @@
 </template>
 
 <script>
+import Store from '@/store'
+
 export default {
   name: 'App',
   watch: {
     '$vuetify.theme.dark' (value) {
       localStorage.setItem('com.pxeger.tome.ui.dark', value.toString())
     }
+  },
+  mounted () {
+    // refresh login token every 5 minutes
+    window.setInterval(() => Store.dispatch('login/doRefresh'), 300)
   }
 }
 </script>
