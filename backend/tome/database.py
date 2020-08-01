5b128e9a35b2a60731fb05e10ee3312732b704cd
pxeger <_@pxeger.com>
2020-08-01 08:22:44 +0200

    add database helper for dynamically constructed update queries

--- backend/tome/database.py
+++ backend/tome/database.py
@@ -1,9 +1,10 @@
 import uuid
-from typing import Any
+from typing import Any, Callable, Iterable
 
 import asyncpg  # type: ignore
 
 from tome import settings
+from tome.exceptions import HTTPException
 
 _connection: asyncpg.Connection = None
 
@@ -50,3 +51,58 @@ async def disconnect() -> None:
     if connection:
         await _connection.close()
         _connection = None
+
+
+def dynamic_update_query(
+    table_name: str,
+    columns: Iterable[str],
+    extra_clause: str = "",
+    placeholders: int = 0,
+) -> Callable:
+    """closure. creates a dynamic SQL update query using values provided
+
+    for example:
+        update_user = dynamic_update_query("users", "id", "name", "email", "id = 345")
+        data = {
+            "name": "hello",
+            "email": None
+        }
+        query = update_user(data)
+        await db.execute(*query)
+
+    Same as `update users set id = id, name = 'hello', email = NULL where id = 345`
+
+
+    table_name, columns, and where must be sanitised!!"""
+
+    # format-ception
+    update = ", ".join(map("{} = {{}}".format, columns))
+    template = "update {} set {} {}".format(table_name, update, extra_clause)
+    # template is now something like "update users set id = {}, name = {}, email = {}
+    #   where id = 345"
+
+    # it is then formatted in the closure to be something like this:
+    # "update users set id = id, name = $1, email = $2 where id = 345"
+
+    def inner(data: dict, *extra: Any) -> Iterable[Any]:
+        nonlocal placeholders
+
+        # if there are any provided keys that don't exist as columns
+        if data.keys() - columns:
+            raise HTTPException("invalid types", 422)
+
+        format_args = []
+        values = []
+        for column in columns:
+            if column in data:
+                # format with a postgresql placeholder
+                format_args.append("$" + str(placeholders))
+                values.append(data[column])
+                placeholders += 1
+            else:
+                # format with the column name, to keep the existing stored value
+                format_args.append(column)
+
+        return template.format(*format_args), *extra, *values
+
+    return inner
