b0c0f804c25af844c2fba2c6cf803d296090ccc4
pxeger <_@pxeger.com>
2020-08-02 17:38:46 +0200

    very basic node tree-view prototype

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nodes">
+    <div v-for="[depth, node] in depthCache" :key="node.id" class="node">
+      <div v-for="i in depth" :key="i" class="node-indent"></div>
+      <div class="node-content">
+        <p class="node-text" contenteditable>{{ node.content }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    nodes: [
+      {
+        id: '1',
+        content: 'Hello, World!',
+        parent: null
+      },
+      {
+        id: '2',
+        content: 'Another thing',
+        parent: null
+      },
+      {
+        id: '3',
+        content: 'Child of Hello World',
+        parent: '1'
+      }
+    ],
+    depthCache: []
+  }),
+  methods: {
+    buildDepthCache () {
+      this.depthCache = Array.from(this._traverse(null, 0))
+    },
+    _traverse: function * (targetParentId, depth) {
+      for (const node of this.nodes) {
+        if (node.parent === targetParentId) {
+          yield [depth, node]
+          yield * this._traverse(node.id, depth + 1)
+        }
+      }
+    }
+  },
+  mounted () {
+    this.buildDepthCache()
   }
 }
 </script>
+
+<style lang="scss">
+.node {
+  width: 100%;
+  display: flex
+}
+.node-indent {
+  width: 1em;
+  height: 1em;
+}
+.node-content {
+  flex-grow: 1;
+}
+</style>
