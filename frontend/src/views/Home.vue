b1be043805db8fd299a198fec0db1d1d0715cb14
pxeger <_@pxeger.com>
2020-08-16 20:14:15 +0100

    add context menu

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,13 +2,44 @@
   <v-app>
     <v-main app>
       <div id="nodes">
-        <div v-for="[depth, node] in depthCache" :key="node.id" class="node">
-          <div v-for="i in depth" :key="i" class="node-indent"></div>
-          <div class="node-content">
-            <p class="node-text" contenteditable>{{ node.content }}</p>
+        <div
+          v-for="[depth, id] in depthCache"
+          :key="id"
+          :id="id"
+          :node="id"
+          class="node"
+          @contextmenu="onContextMenu"
+        >
+          <div v-for="i in depth" :key="i" class="node-indent" :node="id"></div>
+          <div class="node-content" :node="id">
+            <p class="node-text" contenteditable :node="id">{{ nodes[id].content }}</p>
           </div>
         </div>
       </div>
+      <div
+        id="nodes-context-menu"
+        v-if="contextMenuOpen"
+        :style="`top: ${contextMenuY}px; left: ${contextMenuX}px`"
+      >
+        <v-list>
+          <v-list-item href="#">
+            <v-list-item-icon>
+              <v-icon>mdi-delete</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Delete</v-list-item-title>
+          </v-list-item>
+          <v-list-item :href="'#' + eventTarget">
+            <v-list-item-title>
+              <code>{{ eventTarget }}</code>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </div>
+      <div
+        id="nodes-context-menu-blocker"
+        v-if="contextMenuOpen"
+        @mousedown.prevent.stop="closeContextMenu"
+      ></div>
     </v-main>
     <Footer>
       <v-btn icon to="/settings/account" small>
@@ -24,23 +55,27 @@ import Footer from '@/components/Footer.vue'
 export default {
   name: 'Home',
   data: () => ({
-    nodes: [
-      {
-        id: '1',
+    contextMenuOpen: false,
+    contextMenuX: 0,
+    contextMenuY: 0,
+    eventTarget: null,
+    nodes: {
+      abc: {
+        id: 'abc',
         content: 'Hello, World!',
         parent: null
       },
-      {
-        id: '2',
+      def: {
+        id: 'def',
         content: 'Another thing',
         parent: null
       },
-      {
-        id: '3',
+      ghi: {
+        id: 'ghi',
         content: 'Child of Hello World',
-        parent: '1'
+        parent: 'abc'
       }
-    ],
+    },
     depthCache: []
   }),
   methods: {
@@ -48,12 +83,28 @@ export default {
       this.depthCache = Array.from(this._traverse(null, 0))
     },
     _traverse: function * (targetParentId, depth) {
-      for (const node of this.nodes) {
-        if (node.parent === targetParentId) {
-          yield [depth, node]
-          yield * this._traverse(node.id, depth + 1)
+      for (const id in this.nodes) {
+        if (this.nodes[id].parent === targetParentId) {
+          yield [depth, id]
+          yield * this._traverse(id, depth + 1)
         }
       }
+    },
+    onContextMenu (event) {
+      if (this.contextMenuOpen) {
+        this.contextMenuOpen = false
+        this.eventTarget = null
+      } else {
+        event.preventDefault()
+        this.eventTarget = event.target.getAttribute('node')
+        this.contextMenuOpen = true
+        this.contextMenuX = event.pageX
+        this.contextMenuY = event.pageY
+      }
+    },
+    closeContextMenu () {
+      this.contextMenuOpen = false
+      this.eventTarget = null
     }
   },
   mounted () {
@@ -67,6 +118,19 @@ export default {
 #nodes {
   height: 100%;
   margin: 1em;
+}
+#nodes-context-menu-blocker {
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  opacity: 0;
+  z-index: 100;
+  left: 0;
+  top: 0;
+}
+#nodes-context-menu {
+  position: absolute;
+  z-index: 200;
 }
 .node {
   width: 100%;
